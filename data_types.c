a43e115b66d107c78c223143290a70e12a77fcd0
JSenen <juansenen@hotmail.com>
2024-01-09 13:54:00 +0100

    Learning

---
+++ data_types.c
@@ -0,0 +1,16 @@
+#define SIZE 6
+
+void main()
+{
+int i;
+  long int j;
+  long k;
+  short int l;
+  short m;
+  char p  = 'M'; 
+  float a = 2.5;
+  double b = 3.45678;
+  printf("%c\n",'M'+25);
+
+
+}
